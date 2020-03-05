0f27c2fece0ba8f37591dbdf997e76416ef24e36
lahawkin <lhawk1000@gmail.com>
2020-03-05 09:07:00 -0800

    i have the sign up form component, but I don't have routes so they're all on the same page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 //import "./App.css";
 import NewPost from "./newpost.js";
+import SignupForm from "./comp/sign-up.js"
 // import SignInSide from "./oldPost";
 
 function NewProduct() {
   return (
     <div>
       <NewPost />
+      <SignupForm/>
     </div>
   );
 }
