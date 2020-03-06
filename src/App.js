3ae06dd7023bbf1c8d3795c14a6445b1273cfc15
lahawkin <lhawk1000@gmail.com>
2020-03-05 18:48:41 -0800

    moved some files around

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-//import "./App.css";
-import NewPost from "./newpost.js";
+import NewPost from "./comp/newpost.js/index.js";
 import SignupForm from "./comp/sign-up.js"
 import MyNavBar from "./comp/navbar.js"
-// import SignInSide from "./oldPost";
 
-function NewProduct() {
+
+
+
+function App() {
   return (
     <div>
       <MyNavBar />
