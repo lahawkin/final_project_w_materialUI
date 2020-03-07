56e489e76e327720e06c600d8b9d3d5893ced91b
Rodrigo <rodrigo@sdsharp.com>
2020-03-07 13:51:13 -0800

    removed warnings

--- src/comp/newpost.js
+++ src/comp/newpost.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import "../App.css"
+import "../App.css";
 
 function NewPost() {
   return (
