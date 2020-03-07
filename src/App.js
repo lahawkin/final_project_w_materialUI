0a0888a1103adfc6ac389b8b88a59749412c0c42
Rodrigo <rodrigo@sdsharp.com>
2020-03-07 10:30:48 -0800

    inor changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import MyNavBar from "./comp/navbar.js"
-
-
-
+import MyNavBar from "./comp/navbar.js";
+import Album from "./comp/Dashboard";
+import SignIn from "./comp/sign-upMatUI";
 
 function App() {
   return (
     <div>
       <MyNavBar />
-      
+      <SignIn />
+      <Album />
     </div>
   );
 }
